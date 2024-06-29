6e6b2b0897d9cd6931ed0ed7febc18df7849c34d
Jaydip591 <prajapatijaydip591@gmail.com>
2024-06-29 19:04:56 +0530

    all files

--- app.py
+++ app.py
@@ -1,164 +1,84 @@
-from flask import Flask, render_template, url_for, redirect
-from flask_sqlalchemy import SQLAlchemy 
-from flask_login import UserMixin  
-from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import InputRequired, length, ValidationError
-from flask_bcrypt import Bcrypt
+from flask import Flask, render_template, request
+import os
+from langchain.prompts import PromptTemplate
+from langchain.llms import OpenAI
+from langchain.chains import LLMChain
+import re
+
+import os
+import re
+os.environ['OPENAI_API_KEY'] = 'your key is here' # your openai key
+
+
+
 
 
 app = Flask(__name__)
-bcrypt=Bcrypt(app)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///mydatabase.db'
-app.config['SECRET_KEY'] = 'thisisasecretkey'
-db = SQLAlchemy(app) 
 
-class User(db.Model, UserMixin):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(20), nullable=False, unique=True)
-    password = db.Column(db.String(80), nullable=False)
+llm_resto = OpenAI(temperature=0.6)
+prompt_template_resto = PromptTemplate(
+    input_variables=['age', 'gender', 'weight', 'height', 'veg_or_nonveg', 'disease', 'region', 'allergics', 'foodtype'],
+    template="Diet Recommendation System:\n"
+             "I want you to recommend 6 restaurants names, 6 breakfast names, 5 dinner names, and 6 workout names, "
+             "based on the following criteria:\n"
+             "Person age: {age}\n"
+             "Person gender: {gender}\n"
+             "Person weight: {weight}\n"
+             "Person height: {height}\n"
+             "Person veg_or_nonveg: {veg_or_nonveg}\n"
+             "Person generic disease: {disease}\n"
+             "Person region: {region}\n"
+             "Person allergics: {allergics}\n"
+             "Person foodtype: {foodtype}."
+)
 
-class RegisterForm(FlaskForm):
-    username = StringField(validators=[InputRequired(), length(min=4, max=20)], render_kw={"placeholder": "Username"})
 
-    password = PasswordField(validators=[InputRequired(), length(min=8, max=20)], render_kw={"placeholder": "Password"})
 
-    submit = SubmitField('Register')
-class LoginForm(FlaskForm):
-    username = StringField(validators=[InputRequired(), length(min=4, max=20)], render_kw={"placeholder": "Username"})
-
-    password = PasswordField(validators=[InputRequired(), length(min=8, max=20)], render_kw={"placeholder": "Password"})
-
-    submit = SubmitField('login')
-def validate_username(self, username):
-        existing_user_username = User.query.filter_by(username=username.data).first()
-        if existing_user_username:raise ValidationError('That username already exists. Please choose a different one.')
 @app.route('/')
-def home():
-    return render_template('home.html')
+def index():
+    return render_template('index.html')
 
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    form =LoginForm()
 
-    return render_template('login.html', form=form)
+@app.route('/recommend', methods=['POST'])
+def recommend():
+    if request.method == "POST":
+        age = request.form['age']
+        gender = request.form['gender']
+        weight = request.form['weight']
+        height = request.form['height']
+        veg_or_noveg = request.form['veg_or_nonveg']
+        disease = request.form['disease']
+        region = request.form['region']
+        allergics = request.form['allergics']
+        foodtype = request.form['foodtype']
 
-@app.route('/register', methods=['GET', 'POST'])
-def register():
-    form= RegisterForm()
-    if form.validate_on_submit():
-        hashed_password = bcrypt.generate_password_hash(form.password.data)
-        new_user = User(username=form.username.data, password=hashed_password)
-        db.session.add(new_user)
-        db.session.commit()
-        return redirect(url_for('login'))
-    return render_template('register.html', form=form)
+        chain_resto = LLMChain(llm=llm_resto, prompt=prompt_template_resto)
+        input_data = {'age': age,
+                              'gender': gender,
+                              'weight': weight,
+                              'height': height,
+                              'veg_or_nonveg': veg_or_noveg,
+                              'disease': disease,
+                              'region': region,
+                              'allergics': allergics,
+                              'foodtype': foodtype}
+        results = chain_resto.run(input_data)
 
-@app.cli.command("init_db")
-def init_db():
-    """Initialize the database."""
-    db.create_all()
-    print("Database and tables created")
+        # Extracting the different recommendations using regular expressions
+        restaurant_names = re.findall(r'Restaurants:(.*?)Breakfast:', results, re.DOTALL)
+        breakfast_names = re.findall(r'Breakfast:(.*?)Dinner:', results, re.DOTALL)
+        dinner_names = re.findall(r'Dinner:(.*?)Workouts:', results, re.DOTALL)
+        workout_names = re.findall(r'Workouts:(.*?)$', results, re.DOTALL)
+
+        # Cleaning up the extracted lists
+        restaurant_names = [name.strip() for name in restaurant_names[0].strip().split('\n') if name.strip()]
+        breakfast_names = [name.strip() for name in breakfast_names[0].strip().split('\n') if name.strip()]
+        dinner_names = [name.strip() for name in dinner_names[0].strip().split('\n') if name.strip()]
+        workout_names = [name.strip() for name in workout_names[0].strip().split('\n') if name.strip()]
+
+        return render_template('result.html', restaurant_names=restaurant_names,breakfast_names=breakfast_names,dinner_names=dinner_names,workout_names=workout_names)
+    return render_template('index.html')
+
 
 if __name__ == '__main__':
-    app.run(debug=True)
-from flask import Flask, render_template, url_for, redirect
-from flask_sqlalchemy import SQLAlchemy
-from flask_login import UserMixin, login_user, LoginManager, login_required, logout_user, current_user
-from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import InputRequired, Length, ValidationError
-from flask_bcrypt import Bcrypt
-
-app = Flask(__name__)
-db = SQLAlchemy(app)
-bcrypt = Bcrypt(app)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
-app.config['SECRET_KEY'] = 'thisisasecretkey'
-
-
-login_manager = LoginManager()
-login_manager.init_app(app)
-login_manager.login_view = 'login'
-
-
-@login_manager.user_loader
-def load_user(user_id):
-    return User.query.get(int(user_id))
-
-
-class User(db.Model, UserMixin):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(20), nullable=False, unique=True)
-    password = db.Column(db.String(80), nullable=False)
-
-
-class RegisterForm(FlaskForm):
-    username = StringField(validators=[InputRequired(), Length(min=4, max=20)], render_kw={"placeholder": "Username"})
-
-    password = PasswordField(validators=[InputRequired(), Length(min=8, max=20)], render_kw={"placeholder": "Password"})
-
-    submit = SubmitField('Register')
-
-    def validate_username(self, username):
-        existing_user_username = User.query.filter_by(
-            username=username.data).first()
-        if existing_user_username:
-            raise ValidationError(
-                'That username already exists. Please choose a different one.')
-
-
-class LoginForm(FlaskForm):
-    username = StringField(validators=[InputRequired(), Length(min=4, max=20)], render_kw={"placeholder": "Username"})
-
-    password = PasswordField(validators=[InputRequired(), Length(min=8, max=20)], render_kw={"placeholder": "Password"})
-
-    submit = SubmitField('Login')
-
-
-@app.route('/')
-def home():
-    return render_template('home.html')
-
-
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    form = LoginForm()
-    if form.validate_on_submit():
-        user = User.query.filter_by(username=form.username.data).first()
-        if user:
-            if bcrypt.check_password_hash(user.password, form.password.data):
-                login_user(user)
-                return redirect(url_for('dashboard'))
-    return render_template('login.html', form=form)
-
-
-@app.route('/dashboard', methods=['GET', 'POST'])
-@login_required
-def dashboard():
-    return render_template('dashboard.html')
-
-
-@app.route('/logout', methods=['GET', 'POST'])
-@login_required
-def logout():
-    logout_user()
-    return redirect(url_for('login'))
-
-
-@ app.route('/register', methods=['GET', 'POST'])
-def register():
-    form = RegisterForm()
-
-    if form.validate_on_submit():
-        hashed_password = bcrypt.generate_password_hash(form.password.data)
-        new_user = User(username=form.username.data, password=hashed_password)
-        db.session.add(new_user)
-        db.session.commit()
-        return redirect(url_for('login'))
-
-    return render_template('register.html', form=form)
-
-
-if __name__ == "__main__":
     app.run(debug=True)
